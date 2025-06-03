99eef30eef708a952f4606683ec7eb342c470861
PsychicNull <149969907+PsychicNull@users.noreply.github.com>
2025-06-03 15:32:29 -0400

    Added preview config

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,31 @@
 import { Inter, Space_Grotesk } from 'next/font/google'
 import '@/styles/tailwind.css'
+import Header from '@/components/Header'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '@prismicio/client'
+
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
+    subsets: ['latin'],
+    variable: '--font-inter',
 })
 
 const space_grotesk = Space_Grotesk({
-  weight: ['600'],
-  subsets: ['latin'],
-  variable: '--font-space-grotesk',
+    weight: ['600'],
+    subsets: ['latin'],
+    variable: '--font-space-grotesk',
 })
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <div className={`${inter.variable} ${space_grotesk.variable}`}>
-          {children}
-        </div>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body>
+                <div className={`${inter.variable} ${space_grotesk.variable}`}>
+                    <Header />
+                    {children}
+                    <PrismicPreview repositoryName='repositoryName' />
+                </div>
+            </body>
+        </html>
+    )
 }
